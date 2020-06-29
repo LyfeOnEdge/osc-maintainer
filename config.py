67e678a06d7dbcbfab2bc19d30f8ad3de93785f8
LyfeOnEdge <lyfeshomebrew@gmail.com>
2020-06-29 15:23:19 -0700

    add script

---
+++ config.py
@@ -0,0 +1,5 @@
+ignored_directories = [".git"]
+output_directory = "public",
+valid_binary_extensions = (".nro", ".elf", ".rpx")
+domain = "https://hbb1.oscwii.org/hbb"
+metadata_folder = "metadata"
